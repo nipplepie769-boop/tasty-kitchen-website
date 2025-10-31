74f8179cb34d4318b88a2a830b90a6304e4583d0
nipplepie769-boop <nipplepie769@gmail.com>
2025-10-31 15:36:06 +0530

    Rebuild for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  base: '/',
+  base: './', // ✅ fixes blank page on Netlify
 
   server: {
     host: "::",
